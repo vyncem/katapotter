84aab3562e33774704c970579784a885bbb24e9e
vyncem <vyncem@gmail.com>
2018-01-25 10:59:00 +0000

    add red test for purchasing 1 book

--- spec/buy_books_spec.rb
+++ spec/buy_books_spec.rb
@@ -1,4 +1,12 @@
 require './buy_books.rb'
 
 RSpec.describe BuyBooks do
+  subject { BuyBooks.run(book_list) }
+
+  describe 'One copy of any of the five books costs 8 EUR' do
+    let(:book_list) { [1] }
+    let(:exptected_price) { 8 }
+
+    it { is_expected.to eql expected_price }
+  end
 end

0a2aee47747291bd605dc8d8de6f4dea316fe78a
vyncem <vyncem@gmail.com>
2018-01-25 12:02:43 +0000

    refactor run method

--- buy_books.rb
+++ buy_books.rb
@@ -3,28 +3,35 @@
 # calculate the price given a basket/list of books purchased
 class BuyBooks
   PRICE_PER_BOOK = 8
-  DISCOUNT_FOR_2_BOOKS = 0.95
-  DISCOUNT_FOR_3_BOOKS = 0.9
+  DISCOUNT = {
+    '2' => 0.95,
+    '3' => 0.9
+  }.freeze
 
   def initialize(book_list)
     @book_list = book_list
   end
 
   def run
-    case uniq
-    when 2
-      (uniq * PRICE_PER_BOOK * DISCOUNT_FOR_2_BOOKS) \
-      + ((@book_list.size - uniq) * PRICE_PER_BOOK)
-    when 3
-      (uniq * PRICE_PER_BOOK * DISCOUNT_FOR_3_BOOKS) \
-      + ((@book_list.size - uniq) * PRICE_PER_BOOK)
-    else
+    if uniq == 1
       @book_list.size * PRICE_PER_BOOK
+    else
+      discount_price
     end
   end
 
   def uniq
     @book_list.uniq.size
+  end
+
+  private
+
+  def basic_price
+    (@book_list.size - uniq) * PRICE_PER_BOOK
+  end
+
+  def discount_price
+    (uniq * PRICE_PER_BOOK * DISCOUNT[uniq.to_s]) + basic_price
   end
 end
 

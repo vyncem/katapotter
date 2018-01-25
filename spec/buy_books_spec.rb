c685a3e4a488e60d64b9dd2b4cd9ae8c9b3750e8
vyncem <vyncem@gmail.com>
2018-01-25 11:10:54 +0000

    add red test for buying N similar books

--- spec/buy_books_spec.rb
+++ spec/buy_books_spec.rb
@@ -2,10 +2,18 @@ require './buy_books.rb'
 
 RSpec.describe BuyBooks do
   subject { BuyBooks.new(book_list).run }
+  let(:book_price) { 8 }
 
   describe 'One copy of any of the five books costs 8 EUR' do
     let(:book_list) { [1] }
-    let(:expected_price) { 8 }
+    let(:expected_price) { book_price }
+
+    it { is_expected.to eql expected_price }
+  end
+
+  describe 'N copies of a of a book cost N*8 EUR' do
+    let(:book_list) { [1, 1, 1, 1] }
+    let(:expected_price) { book_list.size * book_price }
 
     it { is_expected.to eql expected_price }
   end

c39870c265b2c335f9a8d5bfac40489da08925bd
vyncem <vyncem@gmail.com>
2018-01-25 11:32:41 +0000

    add two book discount

--- buy_books.rb
+++ buy_books.rb
@@ -3,13 +3,23 @@
 # calculate the price given a basket/list of books purchased
 class BuyBooks
   PRICE_PER_BOOK = 8
+  DISCOUNT_FOR_2_BOOKS = 0.95
 
   def initialize(book_list)
     @book_list = book_list
   end
 
   def run
-    @book_list.size * PRICE_PER_BOOK
+    case uniq
+    when 2
+      @book_list.size * PRICE_PER_BOOK * DISCOUNT_FOR_2_BOOKS
+    else
+      @book_list.size * PRICE_PER_BOOK
+    end
+  end
+
+  def uniq
+    @book_list.uniq.size
   end
 end
 
--- spec/buy_books_spec.rb
+++ spec/buy_books_spec.rb
@@ -2,19 +2,31 @@ require './buy_books.rb'
 
 RSpec.describe BuyBooks do
   subject { BuyBooks.new(book_list).run }
-  let(:book_price) { 8 }
-
   describe 'One copy of any of the five books costs 8 EUR' do
     let(:book_list) { [1] }
     let(:expected_price) { book_price }
 
-    it { is_expected.to eql expected_price }
+    it { is_expected.to eql 8 }
   end
 
   describe 'N copies of a of a book cost N*8 EUR' do
     let(:book_list) { [1, 1, 1, 1] }
-    let(:expected_price) { book_list.size * book_price }
 
-    it { is_expected.to eql expected_price }
+    it { is_expected.to eql 32 }
+  end
+
+  describe 'you get a 5% discount on two different books' do
+    let(:book_list) { [1, 2] }
+
+    it { is_expected.to eql 15.2 }
+  end
+
+  describe '#uniq' do
+    context 'when list has 2 different books' do
+      subject { BuyBooks.new(book_list).uniq }
+      let(:book_list) { [1, 2, 2] }
+
+      it { is_expected.to be 2 }
+    end
   end
 end
